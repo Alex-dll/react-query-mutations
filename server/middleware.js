8b59eaa2870d90baa9f949f7f1741aa48aa33b65
Alex-dll <alexsandrojr.303@gmail.com>
2022-06-14 17:38:20 -0300

    finish

--- server/middleware.js
+++ server/middleware.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
   if (req.method.toLowerCase() === "post") {
     setTimeout(() => {
+      // rest.status(400).send();
       next();
     }, 3000);
   } else {
